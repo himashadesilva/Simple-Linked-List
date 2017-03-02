b97523706d2a9f5bc989b3efefe985c47d54fd89
himashadesilva <himashasds@gmail.com>
2017-03-02 16:21:39 +0530

    create nodes

---
+++ src/MyList.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Himasha De Silva on 3/2/2017.
+ */
+//import java.util.NoSuchElementException;
+
+public class MyList<E> {
+
+    private class Node {
+
+        E elem;
+        Node next;
+
+        public Node(E elem) {
+            this.elem = elem;
+        }
+
+        public Node(E el, Node node) {
+            this.elem = el;
+            this.next = node;
+        }
+
+    }
+
+
+
+
+}
