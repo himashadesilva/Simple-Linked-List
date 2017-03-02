1f07531490686ab4438959f163ff4dbbbda8e369
himashadesilva <himashasds@gmail.com>
2017-03-02 16:43:34 +0530

    add method, isEmpty method

--- src/MyList.java
+++ src/MyList.java
@@ -3,23 +3,48 @@
  */
 //import java.util.NoSuchElementException;
 
-public class MyList<E> {
+public class MyList<T> {
+
+    private Node head; // the first element
+
+    private Node tail; // the last element
+
 
     private class Node {
 
-        E elem;
+        T elem;
         Node next;
 
-        public Node(E elem) {
+        public Node(T elem) {
             this.elem = elem;
         }
 
-        public Node(E el, Node node) {
-            this.elem = el;
+        public Node(T t, Node node) {
+            this.elem = t;
             this.next = node;
         }
 
     }
+
+    public void add(T t){
+
+        if(head == null){
+
+            head = tail = new Node(t);
+
+        }
+        else{
+            tail.next = new Node(t);
+            tail = tail.next;
+        }
+
+    }
+
+    public boolean isEmpty(){
+
+        return head == null;
+    }
+
 
 
 }
